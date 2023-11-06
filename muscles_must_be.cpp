4fb9ca009535748f975502b4eb27894f0c30885d
jecxy <jecxy@UFABOYZ>
2023-11-06 17:57:06 +0300

    Добавьте файлы проекта.

---
+++ muscles_must_be.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    int hight;
+    int age;
+    int weight;
+    int calories;
+    std::cout << "Input hight: ";
+    std::cin >> hight;
+    std::cout << "Input age: ";
+    std::cin >> age;
+    std::cout << "Input weight: ";
+    std::cin >> weight;
+    calories = (10 * weight) + (6, 25 * hight )- (5 * age) + 5;
+    std::cout <<"You should eat " << calories<<" calories";
+}
